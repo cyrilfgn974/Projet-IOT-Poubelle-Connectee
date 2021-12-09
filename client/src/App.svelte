c5238ebde762631528b686d0f2652aa04bfca37f
cyrilfgn974 <cyril.figuin@outlook.com>
2021-12-09 09:47:41 +0100

    STRIIIIIIP

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,15 +1,32 @@
 <script>
+
+
   let codeBarre;
-  let nomProduit = "Nul";
-  function getNameFromAPI(name) {
-    fetch("./api/getproduit/" + codeBarre + ".json")
-      .then((d) => d.text())
-      .then((d) => (nomProduit = d));
+  let nomProduit;
+  let src;
+  function launch() {
+    getNameFromAPI();
+    getImageFromAPI();
+
   }
+  function getNameFromAPI() {
+    fetch("./api/getproduitname/" + codeBarre + ".json")
+      .then((d) => d.text())
+      .then((d) => (nomProduit = d))
+  }
+  function getImageFromAPI() {
+    fetch("./api/getproduitimage/" + codeBarre + ".json")
+      .then((d) => d.text())
+      .then((d) => (src = d))
+  }
+
 </script>
 
 <input bind:value={codeBarre} />
-<button on:click={getNameFromAPI}>Search product</button>
-{#if getNameFromAPI}
+<button on:click={launch}>Search product</button>
+{#if launch}
   <p>name of the product: {nomProduit}</p>
+  <p>link img produit : {src}</p>
+  <img src={src} alt=''>
+
 {/if}
--- server.py
+++ server.py
@@ -15,12 +15,25 @@ def base():
 def home(path):
     return send_from_directory('client/public', path)
 
-@app.route("/api/getproduit/<string:codeBarre>.json", methods=["GET"])
-def get_by_name(codeBarre):
+
+def from_off(codeBarre):
     r = requests.get('https://world.openfoodfacts.org/api/v0/product/'+codeBarre+'.json')
     resp = r.json()
+    return resp
+
+@app.route("/api/getproduitname/<string:codeBarre>.json", methods=["GET"])
+def get_name(codeBarre):
+    resp = from_off(codeBarre)
     nomProduit = resp['product']['product_name']
     return json.dumps(nomProduit)
+
+@app.route("/api/getproduitimage/<string:codeBarre>.json", methods=["GET"])
+def get_image(codeBarre):
+    resp = from_off(codeBarre)
+    lienImage = resp['product']['image_front_url']
+    return json.dumps(lienImage).strip("\"")
+
+
     
 if __name__ == "__main__":
     app.run(port=80)
